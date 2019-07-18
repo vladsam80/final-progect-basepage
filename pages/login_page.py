a98ffaa2eb542266bc5edbb24bdf74ce111411db
vladsam80 <52978774+vladsam80@users.noreply.github.com>
2019-07-19 00:53:52 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,14 +7,11 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
-
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "There is no login in the link"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not presented"
