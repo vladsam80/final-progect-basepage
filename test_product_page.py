5194a1dea046c09462b2dff73257c617df5468fa
vladsam80 <52978774+vladsam80@users.noreply.github.com>
2019-07-20 02:36:05 +0300

    New link add

--- test_product_page.py
+++ test_product_page.py
@@ -18,7 +18,7 @@ import time
 '''
 @pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_the_cart()
